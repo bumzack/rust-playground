f73c8c0cb30bb270b16da91c894dddab57e33aa0
bumzack <bumzacktrara@gmail.com>
2015-07-11 19:59:42 +0200

    added more threads

--- threads/threads.rs
+++ threads/threads.rs
@@ -2,10 +2,10 @@ use std::thread;
 use std::sync::{Arc, Mutex};
 
 fn main() {
-    let numbers = Arc::new(Mutex::new(vec![1, 2, 3]));
+    let numbers = Arc::new(Mutex::new(vec![1, 2, 3,4,5,6,7,8,9,10,11,12]));
 
     let mut threads = vec![];
-    for i in 0..3 {
+    for i in 0..12 {
         let number = numbers.clone();
 
         let cur = thread::spawn(move|| {
