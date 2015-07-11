cb085c344c837df1c581a119867f8249784c49f4
bumzack <bumzacktrara@gmail.com>
2015-07-11 20:53:35 +0200

    2 way communication +/- working

--- threads/threads.rs
+++ threads/threads.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::sync::mpsc::channel;
+use std::sync::mpsc::{Sender, Receiver};
 
 
 fn main () {
@@ -7,15 +8,42 @@ fn main () {
     // where tx is the sending half (tx for transmission), and rx is the receiving
     // half (rx for receiving).
     let (tx, rx) = channel();
+ 
+
+    let mut senderlist: Vec<Sender<i32>> = vec![];
+    
+
     for i in 0..10 {
         let tx = tx.clone();
+ 
+        let (tx2, rx2): (Sender<i32>, Receiver<i32>) = channel();
+        senderlist.push(tx2);
+
         thread::spawn(move|| {
+            let dur = i * 800 + 1500;    
+            println!("pause duration  {:?} ms for thread  {:?} ", dur, i);
+
+            println!("start sleeping for {:?} ms in thread {:?}", dur, i);
+            thread::sleep_ms(dur);
+            println!("finished sleeping for {:?} ms in thread {:?}", dur, i);
+
             tx.send(i+1).unwrap();
+            println!("in thread {:?}", rx2.recv().unwrap());
+            
         });
     }
 
+    for k in 0..10 {
+        let mut z = k as i32;
+        z = z + 100;  
+        println!("sending z = {:?} from main thread to 'spawned' thread, k = {:?}", z, k);
+        senderlist[k].send(z).unwrap();
+    }  
+
     for _ in 0..10 {
         let j = rx.recv().unwrap();
+        println!("'main thread' received {:?} from spawned thread", j);
+
         assert!(0 <= j && j < 11);
     }    
 }
