26bb70f47778bd050f4fd449058b92bb12484644
bumzack <bumzacktrara@gmail.com>
2015-07-15 16:15:14 +0200

    looking good. can add different trait implementations to an array

--- traits/imageops.rs
+++ traits/imageops.rs
@@ -20,6 +20,13 @@ struct ImageOperationReplaceColor  {
     description: &'static str,
 }
 
+struct ImageOperationSharpenColor  {
+    sharpen_factor: f64,
+
+    name: &'static str,
+    description: &'static str,
+}
+
 trait ImageOperation {
     fn execute_op(&self) -> ImageOperationResult;
 
@@ -28,7 +35,7 @@ trait ImageOperation {
 }
 
 impl ImageOperation for ImageOperationReplaceColor {
-    // Replace `Self` with the implementor type: `Dog`
+    // Replace `Self` with the implementor type: `ImageOperationReplaceColor`
     fn execute_op(&self) -> ImageOperationResult {
 
          let res = ImageOperationResult {
@@ -54,6 +61,41 @@ impl ImageOperation for ImageOperationReplaceColor {
     }
 }
 
+//impl ImageOperationSharpenColor {
+//    fn new (&self) {
+        //self.name = "Image Sharpen Filter";
+        //self.description = "Image Sharpen Filter - i am the descirptipn of the Image Sharpen Filter";
+//    }
+//}
+
+impl ImageOperation for ImageOperationSharpenColor {
+    // Replace `Self` with the implementor type: `ImageOperationSharpenColor`
+    fn execute_op(&self) -> ImageOperationResult {
+
+         let res = ImageOperationResult {
+            // Static methods are called using double colons
+            width: 0,
+            height: 0,
+            startx: 0,
+            starty: 0,
+            subimage : PixelImageSimple { width: 0, height: 0, pixels: Vec::new() }
+        };
+        println!("impl ImageOperation for ImageOperationSharpenColor -> executeOp");
+
+        // return res as function result
+        res
+    }
+
+    fn description(&self) -> &'static str {
+        self.description
+    }
+
+    fn name(&self) -> &'static str {
+        self.name
+    }
+}
+
+
 
 // thx to http://stackoverflow.com/questions/25818082/vector-of-objects-belonging-to-a-trait
 struct Image {
@@ -62,12 +104,18 @@ struct Image {
 }
 
 impl Image {
+
     fn new() {
         println!("impl Image -> new() - initialize the image_operations array");
         //self.image_operations = Vec::new();
     }
 
-}
+    // add an image operation to the array of image operation
+    fn add_op(&self, image_op: Box<ImageOperation>) {
+        println!("impl Image -> add_op() - adding the following image_op:  {}", image_op.name());
+
+     }
+ }
 
 fn main () {
     let imagesimple = PixelImageSimple {
@@ -77,6 +125,21 @@ fn main () {
         pixels: Vec::new()
     };
 
+    let sharpen_filter_op = Box::new(ImageOperationSharpenColor {
+            name : "Sharpen Filter",
+            description: "Sharpen Filter - description",
+            sharpen_factor: 0.67
+    });
+
+    let replace_color_op = Box::new(ImageOperationReplaceColor {
+            name : "Replace Color Filter",
+            description: "Replace Color  Filter - description",
+            color_old: 0.12,
+            color_new: 0.44
+    });
+
     let image = Image { image_operations: Vec::new() };
 
-}
+    image.add_op(sharpen_filter_op);
+    image.add_op(replace_color_op);
+ }
