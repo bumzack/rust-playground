cf12a9689456237745d7b51695c8c8817e0a684c
bumzack <bumzacktrara@gmail.com>
2015-07-15 15:15:41 +0200

    array of traits seems to work - use "Box" as Vec neeeds to know the size of the element it stores. and different implementations for a trait can be of a different size Box is always the same size
    see: http://stackoverflow.com/questions/25818082/vector-of-objects-belonging-to-a-trait

--- traits/imageops.rs
+++ traits/imageops.rs
@@ -1,41 +1,82 @@
-struct PixelImage {
+struct PixelImageSimple {
     pixels:  Vec<i32>,
     width: i32,
     height: i32,
 }
 
-impl PixelImage {
-    // This is a static method
-    // Static methods don't need to be called by an instance
-    // These methods are generally used as constructors
-    fn new() -> PixelImage {
-        println!("PixelImage::new()");
-        PixelImage { pixels: Vec::new(), width: 0, height: 0 }
-    }
-}
-
 struct ImageOperationResult  {
-    subimage: PixelImage,
-    width: i32,
+    subimage: PixelImageSimple,
+    width:  i32,
     height: i32,
     startx: i32,
     starty: i32,
 }
 
+struct ImageOperationReplaceColor  {
+    color_old: f64,
+    color_new: f64,
+
+    name: &'static str,
+    description: &'static str,
+}
+
 trait ImageOperation {
-    fn executeOp(&self) -> ImageOperationResult;
+    fn execute_op(&self) -> ImageOperationResult;
+
+    fn name(&self) -> &'static str;
+    fn description(&self) -> &'static str;
+}
+
+impl ImageOperation for ImageOperationReplaceColor {
+    // Replace `Self` with the implementor type: `Dog`
+    fn execute_op(&self) -> ImageOperationResult {
+
+         let res = ImageOperationResult {
+            // Static methods are called using double colons
+            width: 0,
+            height: 0,
+            startx: 0,
+            starty: 0,
+            subimage : PixelImageSimple { width: 0, height: 0, pixels: Vec::new() }
+        };
+        println!("impl ImageOperation for ImageOperationReplaceColor -> executeOp");
+
+        // return res as function result
+        res
+    }
+
+    fn description(&self) -> &'static str {
+        self.description
+    }
+
+    fn name(&self) -> &'static str {
+        self.name
+    }
 }
 
 
-struct ImageOp {
-    Image: PixelImage,
+// thx to http://stackoverflow.com/questions/25818082/vector-of-objects-belonging-to-a-trait
+struct Image {
+    // image_operations: &'static  Vec<ImageOperation>,
+    image_operations: Vec<Box<ImageOperation>>,
+}
+
+impl Image {
+    fn new() {
+        println!("impl Image -> new() - initialize the image_operations array");
+        //self.image_operations = Vec::new();
+    }
+
 }
 
 fn main () {
-    let image = PixelImage {
+    let imagesimple = PixelImageSimple {
         // Static methods are called using double colons
         width: 0,
         height: 0,
         pixels: Vec::new()
     };
+
+    let image = Image { image_operations: Vec::new() };
+
 }
