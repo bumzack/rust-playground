7f82032396f5975cb4430445474a68f1ee71a3c1
bumzack <bumzacktrara@gmail.com>
2015-07-26 00:12:47 +0200

    hm - looking good - but it's too easy for now

--- traits/image_ops_polymorphism.rs
+++ traits/image_ops_polymorphism.rs
@@ -1,11 +1,28 @@
 #![feature(box_syntax)]
 
+struct PixelImageSimple {
+    pixels:  Vec<i32>,
+    width: i32,
+    height: i32,
+}
+
+impl PixelImageSimple  {
+    fn new() -> PixelImageSimple {
+        PixelImageSimple {
+            pixels: vec![],
+            width: 10,
+            height: 10,
+        }
+    }
+}
+
 trait ImageOps {
     fn example_method(&self);
 }
 
 struct ImageOpSharpen {
-    just_some_random_data: i32
+    just_some_random_data: i32,
+    img: PixelImageSimple
 }
 
 struct ImageOpReplaceColor {
@@ -24,7 +41,10 @@ impl ImageOps for ImageOpReplaceColor {
 }
 impl ImageOpSharpen {
     fn new() -> ImageOpSharpen {
-        ImageOpSharpen   { just_some_random_data: 1}
+        ImageOpSharpen   { just_some_random_data: 1, img:PixelImageSimple::new() }
+    }
+    fn new2(image: PixelImageSimple) -> ImageOpSharpen {
+        ImageOpSharpen   { just_some_random_data: 1, img: image}
     }
 }
 
@@ -40,14 +60,17 @@ struct Image<'a> {
 
 impl<'a> Image<'a> {
     fn new() -> Image<'a> {
+        let imagesimple = PixelImageSimple::new();
         let bla = box ImageOpSharpen::new() as Box<ImageOps>;
         let bla2 = box ImageOpReplaceColor::new() as Box<ImageOps>;
+        let bla3 = box ImageOpSharpen::new2(imagesimple) as Box<ImageOps>;
 
-        Image { image_operations: vec![bla, bla2] }
+        Image { image_operations: vec![bla, bla2, bla3] }
     }
 }
 
 fn main() {
+    let imagesimple = PixelImageSimple::new();
         let example = Image::new();
         for thing in example.image_operations.iter() {
                 thing.example_method();
