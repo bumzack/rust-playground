de0d62e401e6d6a2d61fba737c2ff992ff38ec5a
bumzack <bumzacktrara@gmail.com>
2015-07-26 18:23:24 +0200

    removed the "Type ..."  code part - which makes the filename obsolete also tried this: http://stackoverflow.com/questions/27675554/type-definition-with-a-trait-differences-of-specifying-an-explicit-lifetime-bou

--- traits/associated_types_image_ops.rs
+++ traits/associated_types_image_ops.rs
@@ -1,13 +1,11 @@
-trait ImageOp<'a> {
-    type ImageData;
-
-    fn execute_op(&self);
-}
-
 struct PixelImageSimple<'a> {
     pixels: &'a Vec<i32>,
     width: i32,
     height: i32,
+}
+
+trait ImageOperation<'a> {
+    fn execute_op(&self);
 }
 
 struct ImageOperationSharpen<'a> {
@@ -15,51 +13,52 @@ struct ImageOperationSharpen<'a> {
     bitmapdata: &'a PixelImageSimple<'a>
 }
 
-impl<'a> ImageOp<'a> for ImageOperationSharpen<'a> {
-    type ImageData = PixelImageSimple<'a>;
+impl<'a> ImageOperation<'a> for ImageOperationSharpen<'a> {
     fn execute_op(&self) {
-        println!("ImageOperationSharpen - val = {}, width = {}, height = {}, pixels = {:?}", &self.val, &self.bitmapdata.width, &self.bitmapdata.height,&self.bitmapdata.pixels);
+        println!("ImageOperationSharpen - val = {}, width = {}, height = {}, pixels = {:?}",
+            &self.val, &self.bitmapdata.width, &self.bitmapdata.height,&self.bitmapdata.pixels);
     }
 }
 
 struct Image<'a> {
-    // image_operations: Vec<Box<ImageOp<ImageData = PixelImageSimple>>>
-    // image_operations: Vec<Box<&'a (ImageOp<ImageData = PixelImageSimple> + 'a)>>
-    // image_operations: Vec<Box<ImageOp<ImageData = PixelImageSimple<'a>>>>
-    image_operations: Vec<Box<ImageOp<PixelImageSimple +'a>>>
+    image_operations: Vec<Box<ImageOperation<'a>>>
 }
 
-//impl<'a> Image<'a> {
-//  fn new() -> Image<'a> {
-//       Image { image_operations: vec![] }
-//   }
+impl<'a> Image<'a> {
+    fn new() -> Image<'a> {
+        Image { image_operations: vec![] }
+    }
 
-//    fn add_op(&mut self, image_ops: Box<ImageOp<ImageData=PixelImageSimple<'a> >>) {
-//           self.image_operations.push(image_ops);
-//    }
-//}
+    fn add_op(&mut self, image_ops: Box<ImageOperation<'a>>) {
+           self.image_operations.push(image_ops);
+    }
+}
+
+type CollectionOfImageOps<'a> = Vec<&'a (ImageOperation<'a> + 'a)>;
 
 fn main () {
-    let bla = vec![1,2,3];
+    let bitmapdata = vec![1,2,3];
+    let bitmapdata2 = vec![1,2,3];
 
-    let bitmap = PixelImageSimple { pixels: &bla, width: 222, height:334 };
+    let bitmap = PixelImageSimple { pixels: &bitmapdata, width: 222, height:334 };
+    let bitmap2 = PixelImageSimple { pixels: &bitmapdata2, width: 111, height:289 };
 
-    let sharpen = ImageOperationSharpen { val: 23, bitmapdata: &bitmap };
-    let sharpen2 = ImageOperationSharpen { val: 34, bitmapdata: &bitmap };
+    let sharpen     = ImageOperationSharpen { val: 23, bitmapdata: &bitmap };
+    let sharpen2    = ImageOperationSharpen { val: 34, bitmapdata: &bitmap2 };
 
-    let obj = Box::new(sharpen) as Box<ImageOp<ImageData=PixelImageSimple>>;
-    let obj2 = Box::new(sharpen2) as Box<ImageOp<ImageData=PixelImageSimple>>;
+    let mut c2: CollectionOfImageOps = Vec::new();
+    c2.push(&sharpen);
+    c2.push(&sharpen2);
 
-    obj.execute_op();
-    obj2.execute_op();
+/*
+    let mut image = Image::new();
 
-//    let mut image = Image::new();
-
-//    image.add_op(obj);
-//    image.add_op(obj2);
+    image.add_op(box_sharpen);
+    image.add_op(box_sharpen2);
 
     println!("execute_op()");
-//    for imageops in image.image_operations.iter() {
-//        imageops.execute_op();
-//    }
+    for imageops in image.image_operations.iter() {
+        imageops.execute_op();
+    }
+*/
 }
