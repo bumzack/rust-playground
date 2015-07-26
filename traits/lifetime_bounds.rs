af37a34dbfd6adb2f8da6485f84a2097c9654168
bumzack <bumzacktrara@gmail.com>
2015-07-26 19:01:36 +0200

    minimal example

--- traits/lifetime_bounds.rs
+++ traits/lifetime_bounds.rs
@@ -20,6 +20,18 @@ impl<'a> ImageOperation<'a> for ImageOperationSharpen<'a> {
     }
 }
 
+struct ImageOperationRotate<'a> {
+    angle: f64,
+    bitmapdata: &'a PixelImageSimple<'a>
+}
+
+impl<'a> ImageOperation<'a> for ImageOperationRotate<'a> {
+    fn execute_op(&self) {
+        println!("ImageOperationRotate - angle = {}, width = {}, height = {}, pixels = {:?}",
+            &self.angle, &self.bitmapdata.width, &self.bitmapdata.height,&self.bitmapdata.pixels);
+    }
+}
+
 struct Image<'a> {
     image_operations: Vec<Box<ImageOperation<'a>>>
 }
@@ -34,27 +46,22 @@ impl<'a> Image<'a> {
     }
 }
 
-type CollectionOfImageOps<'a> = Vec<&'a (ImageOperation<'a> + 'a)>;
-
 fn main () {
     let bitmapdata = vec![1,2,3];
-    let bitmapdata2 = vec![1,2,3];
 
     let bitmap = PixelImageSimple { pixels: &bitmapdata, width: 222, height:334 };
-    let bitmap2 = PixelImageSimple { pixels: &bitmapdata2, width: 111, height:289 };
 
-    let dummy    = ImageOperationSharpen { val: 34, bitmapdata: &bitmap2 };
-    let sharpen     = ImageOperationSharpen { val: 23, bitmapdata: &bitmap };
+    let sharpen = ImageOperationSharpen { val: 34, bitmapdata: &bitmap };
+    let rotate = ImageOperationRotate { angle: 13.32, bitmapdata: &bitmap };
 
-    let mut c2: CollectionOfImageOps = Vec::new();
-    c2.push(&dummy);
-    c2.push(&sharpen);
+    let box_sharpen = Box::new(sharpen);
+    let box_rotate = Box::new(rotate);
 
 /*
     let mut image = Image::new();
 
     image.add_op(box_sharpen);
-    image.add_op(box_sharpen2);
+    image.add_op(box_rotate);
 
     println!("execute_op()");
     for imageops in image.image_operations.iter() {
