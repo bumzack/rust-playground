f9b4da69839345d1cde6cb2936895fb433c84b73
bumzack <bumzacktrara@gmail.com>
2015-07-26 14:24:36 +0200

    borrow works, if not stored in a vector. so how to solve this problem?

--- traits/associated_types_image_ops.rs
+++ traits/associated_types_image_ops.rs
@@ -1,49 +1,49 @@
-trait ImageOp {
+trait ImageOp<'a> {
     type ImageData;
 
     fn execute_op(&self);
 }
 
-struct PixelImageSimple {
-    pixels: Vec<i32>,
+struct PixelImageSimple<'a> {
+    pixels: &'a Vec<i32>,
     width: i32,
     height: i32,
 }
 
-struct ImageOperationSharpen {
+struct ImageOperationSharpen<'a> {
     val: i32,
-    bitmapdata: PixelImageSimple
+    bitmapdata: &'a PixelImageSimple<'a>
 }
 
-
-impl ImageOp for ImageOperationSharpen {
-    type ImageData = PixelImageSimple;
+impl<'a> ImageOp<'a> for ImageOperationSharpen<'a> {
+    type ImageData = PixelImageSimple<'a>;
     fn execute_op(&self) {
-        println!("ImageOperationSharpen");
+        println!("ImageOperationSharpen - val = {}, width = {}", &self.val, &self.bitmapdata.width);
     }
 }
 
-struct Image<'a> {
-    image_operations: Vec<Box<ImageOp<ImageData=PixelImageSimple> + 'a>>
-}
+//struct Image<'a> {
+//    image_operations: Vec<Box<&'a ImageOp<ImageData=PixelImageSimple> + 'a>>
+//    image_operations: Vec<Box<&'a (ImageOp<ImageData = PixelImageSimple> + 'a)>>
+//}
 
-impl<'a> Image<'a> {
-  fn new() -> Image<'a> {
-       Image { image_operations: vec![] }
-   }
+//impl<'a> Image<'a> {
+//  fn new() -> Image<'a> {
+//       Image { image_operations: vec![] }
+//   }
 
-    fn add_op(&mut self, image_ops: Box<ImageOp<ImageData=PixelImageSimple>>) {
-           self.image_operations.push(image_ops);
-    }
-}
+//    fn add_op(&mut self, image_ops: Box<ImageOp<ImageData=PixelImageSimple<'a> >>) {
+//           self.image_operations.push(image_ops);
+//    }
+//}
 
 fn main () {
+    let bla = vec![1,2,3];
 
-    let bitmap = PixelImageSimple { pixels: vec![1,2,3], width: 3, height:4 };
-    let bitmap2 = PixelImageSimple { pixels: vec![4,5,6], width: 13, height: 14 };
+    let bitmap = PixelImageSimple { pixels: &bla, width: 3, height:4 };
 
-    let sharpen = ImageOperationSharpen { val: 23, bitmapdata: bitmap };
-    let sharpen2 = ImageOperationSharpen { val: 23, bitmapdata: bitmap2 };
+    let sharpen = ImageOperationSharpen { val: 23, bitmapdata: &bitmap };
+    let sharpen2 = ImageOperationSharpen { val: 34, bitmapdata: &bitmap };
 
     let obj = Box::new(sharpen) as Box<ImageOp<ImageData=PixelImageSimple>>;
     let obj2 = Box::new(sharpen2) as Box<ImageOp<ImageData=PixelImageSimple>>;
@@ -51,13 +51,13 @@ fn main () {
     obj.execute_op();
     obj2.execute_op();
 
-    let mut image = Image::new();
+//    let mut image = Image::new();
 
-    image.add_op(obj);
-    image.add_op(obj2);
+//    image.add_op(obj);
+//    image.add_op(obj2);
 
     println!("execute_op()");
-    for imageops in image.image_operations.iter() {
-        imageops.execute_op();
-    }
+//    for imageops in image.image_operations.iter() {
+//        imageops.execute_op();
+//    }
 }
