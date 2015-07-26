846ef7f5a89f4b52d6787203dfa5433c898e3296
bumzack <bumzacktrara@gmail.com>
2015-07-26 13:44:59 +0200

    we are getting there. one big thing missing: use 1 image and borrow it multiple times!

--- traits/associated_types_image_ops.rs
+++ traits/associated_types_image_ops.rs
@@ -12,16 +12,16 @@ struct PixelImageSimple {
 }
 
 struct ImageOperationSharpen {
-  val: i32
+    val: i32,
+    bitmapdata: PixelImageSimple
 }
-
 
 
 impl ImageOp for ImageOperationSharpen {
     type ImageData = PixelImageSimple;
 
     fn saymyname(&self, image_data: &PixelImageSimple) {
-        println!("ImageOperationSharpen");
+    //    println!("ImageOperationSharpen - image_data.width {}", image_data.width);
     }
 
     fn execute_op(&self) {
@@ -36,7 +36,7 @@ struct ImageOperationRotate {
   degree: i32
 }
 
- 
+
 impl ImageOp for ImageOperationRotate {
     type ImageData = PixelImageSimple;
 
@@ -49,8 +49,28 @@ impl ImageOp for ImageOperationRotate {
     }
 }
 
+
+
+struct Image<'a> {
+    image_operations: Vec<Box<ImageOp<ImageData=PixelImageSimple> + 'a>>
+}
+
+impl<'a> Image<'a> {
+  fn new() -> Image<'a> {
+       Image { image_operations: vec![] }
+   }
+
+    fn add_op(&mut self, image_ops: Box<ImageOp<ImageData=PixelImageSimple>>) {
+           self.image_operations.push(image_ops);
+    }
+}
+
 fn main () {
-    let sharpen = ImageOperationSharpen { val: 23 };
+
+    let bitmap = PixelImageSimple { pixels: vec![1,2,3], width: 3, height:4 };
+    let bitmap2 = PixelImageSimple { pixels: vec![4,5,6], width: 13, height: 14 };
+
+    let sharpen = ImageOperationSharpen { val: 23, bitmapdata: bitmap };
     let rotate = ImageOperationRotate { degree: 23 };
 
      let obj = Box::new(sharpen) as Box<ImageOp<ImageData=PixelImageSimple>>;
@@ -59,4 +79,13 @@ fn main () {
     obj.execute_op();
     obj2.execute_op();
 
+
+    let mut image = Image::new();
+
+    image.add_op(obj);
+    image.add_op(obj2);
+
+    for imageops in image.image_operations.iter() {
+        imageops.execute_op();
+    }
 }
