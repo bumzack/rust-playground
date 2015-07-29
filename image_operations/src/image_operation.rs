8781d073faf4ae8f7cc8e8d90e66fa59950981df
bumzack <bumzacktrara@gmail.com>
2015-07-29 22:59:29 +0200

    added new functions to trait

--- image_operations/src/image_operation.rs
+++ image_operations/src/image_operation.rs
@@ -6,4 +6,6 @@ pub trait ImageOperation {
     fn execute_op2(&self, input: &ImageOperationParam) -> ImageOperationParam;
     fn prepare_op(&self) -> Vec<ImageOperationParam>;
     fn merge_results(&self, partial_results: Vec<ImageOperationParam>) -> PixelImageSimple;
+    fn set_input_bitmap(&self, input_bitmap: PixelImageSimple);
+    fn get_output_bitmap(&self) -> PixelImageSimple;
 }
