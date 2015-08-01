b856d239d10a8fe75d73752d6801d446efd99ca3
bumzack <bumzacktrara@gmail.com>
2015-08-01 14:09:22 +0200

    use (&mut self) instead of (&self)

--- image_operations/src/image_operation.rs
+++ image_operations/src/image_operation.rs
@@ -3,9 +3,9 @@ use pixel_image_simple::ImageOperationParam;
 
 pub trait ImageOperation {
     fn execute_op(&self);
-    fn execute_op2(&self, input: &ImageOperationParam) -> ImageOperationParam;
+    fn execute_op2(&mut self, input: &ImageOperationParam) -> ImageOperationParam;
     fn prepare_op(&self) -> Vec<ImageOperationParam>;
     fn merge_results(&self, partial_results: Vec<ImageOperationParam>) -> PixelImageSimple;
     fn set_input_bitmap(&mut self, input_bitmap: PixelImageSimple);
-    fn get_output_bitmap(&self) -> PixelImageSimple;
+    fn get_output_bitmap(&self) -> &PixelImageSimple;
 }
