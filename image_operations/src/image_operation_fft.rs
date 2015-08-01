f5d8f5b60db7b49cd7e5ea7b1a7a955fdbde0d82
bumzack <bumzacktrara@gmail.com>
2015-08-01 23:26:28 +0200

    different fft implementation

--- image_operations/src/image_operation_fft.rs
+++ image_operations/src/image_operation_fft.rs
@@ -3,6 +3,8 @@
 use num::complex::Complex64;
 use std::rc::Rc;
 
+use std::f64::consts;
+
 use pixel_image_simple::PixelImageSimple;
 use pixel_image_simple::ImageOperationParam;
 use pixel_image_simple::RGBA8;
@@ -24,7 +26,7 @@ impl ImageOperation for ImageOperationFFT {
         let res_bitmap: Vec<RGBA8> = vec![];
         let mut res = ImageOperationParam::new2(0, 0, res_bitmap);
 
-        let mut complex_bitmap: Vec<Complex64> = vec![];
+        let mut fft_input: Vec<f64> = vec![];
         let mut amp: Vec<f64> = vec![];
         // let mut phase: Vec<f64> = vec![];
 
@@ -32,37 +34,25 @@ impl ImageOperation for ImageOperationFFT {
 
         for y in 0..self.input_bitmapdata.height {
             for x in 0..self.input_bitmapdata.width {
-                pixel  = self.input_bitmapdata.get_pixel(x, y);
+                pixel = self.input_bitmapdata.get_pixel(x, y);
                 // println!("pixel fft input  {}/{} = r: {}, g: {}, b: {}, a: {}", x, y, pixel.r, pixel.g,  pixel.b,  pixel.a);
 
-                complex_bitmap.push(Complex64{ re: pixel.b as f64, im: 0.0});
+                fft_input.push( pixel.b as f64);
             }
         }
-
-        let fft_res = fft2D(&mut complex_bitmap, self.input_bitmapdata.width, self.input_bitmapdata.height, 1);
-        //println!("ImageOperationFFT::execute_op2   ->  res: {}", fft_res);
-        //println!("ImageOperationFFT::complex_bitmap   ->  res: {:?}", complex_bitmap);
-        // println!("ImageOperationFFT::complex_bitmap   ->  self.input_bitmapdata.width: {}, self.input_bitmapdata.height: {}", self.input_bitmapdata.width, self.input_bitmapdata.height);
-        // println!("ImageOperationFFT::input_bitmapdata   ->  self.input_bitmapdata.pixels: {:?}",  self.input_bitmapdata.pixels);
+        let fft_res = FFT::new().execute_real(fft_input);
 
         let mut idx = 0;
-        //println!("ImageOperationFFT::input_bitmapdata   ->  self.output_bitmapdata.width : {},  self.output_bitmapdata.height : {}",  self.output_bitmapdata.width , self.output_bitmapdata.height );
-
         let mut amp_max = 0.0;
 
         for y in 0..self.output_bitmapdata.height {
             for x in 0..self.output_bitmapdata.width {
                 idx = y * self.output_bitmapdata.width + x;
-                let val = (complex_bitmap[idx as usize].re * complex_bitmap[idx as usize].re  +
-                             complex_bitmap[idx as usize].im * complex_bitmap[idx as usize].im).sqrt().abs();
+                let val = (fft_res[idx as usize].re * fft_res[idx as usize].re  +
+                             fft_res[idx as usize].im * fft_res[idx as usize].im).sqrt().abs();
                 amp.push(val);
 
-                //let val = (complex_bitmap[idx as usize].im / complex_bitmap[idx as usize].re)  +
-                //              complex_bitmap[idx as usize].im * complex_bitmap[idx as usize].im).sqrt().abs();
-
-                //if val > 0.0 {
-                    println!("ImageOperationFFT::input_bitmapdata   {}/{}: val =  {}", x, y, val);
-                //}
+                println!("ImageOperationFFT::input_bitmapdata   {}/{}: val =  {}", x, y, val);
                 if val > amp_max {
                     amp_max = val;
                     println!("ImageOperationFFT::input_bitmapdata  setting amp_max to  {} @ {}/{}", val, x, y);
@@ -81,8 +71,8 @@ impl ImageOperation for ImageOperationFFT {
         for y in 0..self.output_bitmapdata.height {
             for x in 0..self.output_bitmapdata.width {
                 idx = y * self.output_bitmapdata.width + x;
-                //let val = (complex_bitmap[idx as usize].re * complex_bitmap[idx as usize].re  +
-                //             complex_bitmap[idx as usize].im * complex_bitmap[idx as usize].im).sqrt().abs();
+                //let val = (fft_res[idx as usize].re * fft_res[idx as usize].re  +
+                //             fft_res[idx as usize].im * fft_res[idx as usize].im).sqrt().abs();
                 let q = c * (1.0 + amp[idx as usize]).ln();
                 let q8 = q as u8;
                 if amp[idx as usize] > 0.0 {
@@ -90,11 +80,11 @@ impl ImageOperation for ImageOperationFFT {
                 }
 
                 //if q8 > 0 {
-                    pixel = RGBA8 {r:q8, g: q8, b: q8, a: 255};
+                pixel = RGBA8 {r:q8, g: q8, b: q8, a: 255};
                 //} else {
                 //    pixel = RGBA8 {r:255, g: 255, b: 255, a: 255};
                 //}
-                 self.output_bitmapdata.set_pixel(x, y, pixel);
+                self.output_bitmapdata.set_pixel(x, y, pixel);
             }
         }
         println!("ImageOperationFFT::input_bitmapdata   max amplitude: {}", amp_max);
@@ -131,215 +121,73 @@ impl ImageOperation for ImageOperationFFT {
 
 
 
+#[macro_export]
+macro_rules! complex_vec(
+    ($($real:expr, $imaginary:expr),*) => ({
+        let mut _temp = ::std::vec::Vec::new();
+        $(
+            let c = Complex64::new($real, $imaginary);
+            _temp.push(c);
+         )*
+        _temp
+    });
+    ($($e:expr),+,) => (vec!($($e),+))
+);
 
-
-
-
-
-
-
-//-------------------------------------------------------------------------
-//   Calculate the closest but lower power of two of a number
-//   twopm = 2**m <= n
-//   Return TRUE if 2**m == n
-//
-fn power_of_2(n: i32) -> (i32, i32, bool) {
-
-    let mut m: i32 = 0;
-    let mut twopm: i32 = 0;
-    if n <= 1 {
-        return (0, 1, false);
-    }
-
-    m = 1;
-    twopm = 2;
-    loop {
-        m += 1;
-        twopm *= 2;
-        if (2*twopm > n) { break } ;
-    }
-
-    if (twopm != n) {
-        (m, twopm, false)
-    } else {
-        (m, twopm, true)
-    }
-}
-
-/*-------------------------------------------------------------------------
-   This computes an in-place complex-to-complex FFT
-   x and y are the real and imaginary arrays of 2^m points.
-   dir =  1 gives forward transform
-   dir = -1 gives reverse transform
-
-     Formula: forward
-                  N-1
-                  ---
-              1   \          - j k 2 pi n / N
-      X(n) = ---   >   x(k) e                    = forward transform
-              N   /                                n=0..N-1
-                  ---
-                  k=0
-
-      Formula: reverse
-                  N-1
-                  ---
-                  \          j k 2 pi n / N
-      X(n) =       >   x(k) e                    = forward transform
-                  /                                n=0..N-1
-                  ---
-                  k=0
-*/
-fn fft(dir: i32, m: i32,  x: &mut Vec<f64>, y: &mut Vec<f64>) -> bool {
-    let mut nn: usize = 0;
-    let mut i : usize = 0;
-    let mut i1 : usize = 0;
-    let mut j : usize = 0;
-    let mut k : usize = 0;
-    let mut i2 : usize = 0;
-    let mut l : usize = 0;
-    let mut l1 : usize = 0;
-    let mut l2 : usize = 0;
-
-    let mut c1 : f64 = 0.0;
-    let mut c2 : f64 = 0.0;
-    let mut tx : f64 = 0.0;
-    let mut ty : f64 = 0.0;
-    let mut t1 : f64 = 0.0;
-    let mut t2 : f64 = 0.0;
-    let mut u1 : f64 = 0.0;
-    let mut u2 : f64 = 0.0;
-    let mut z : f64 = 0.0;
-
-    // Calculate the number of points
-    nn = 1;
-    for i in 0..m {
-        nn *= 2;
-    }
-
-    i2 = nn >> 1;
-    j = 0;
-
-    for i in 0..(nn-1) {
-        if i < j {
-            tx = x[i];
-            ty = y[i];
-            x[i] = x[j];
-            y[i] = y[j];
-            x[j] = tx;
-            y[j] = ty;
+#[macro_export]
+macro_rules! assert_complex_vec_eq(
+    ($expected:expr, $actual:expr, $tolerance:expr) => (
+        for (i, a) in $actual.iter().enumerate() {
+            let e = $expected[i];
+            let diff = e - *a;
+            assert!(Float::abs(diff.re) < $tolerance && Float::abs(diff.im) < $tolerance);
         }
-        k = i2;
-        while k <= j {
-           j -= k;
-           k = k >> 1;
+    );
+);
+
+pub struct FFT;
+
+impl FFT {
+    pub fn new() -> FFT {
+        let fft = FFT;
+        fft
+    }
+
+    pub fn execute_real(&self, vec: Vec<f64>) -> Vec<Complex64> {
+        let length = vec.len();
+        if length == 1 {
+            return complex_vec!(vec[0],0f64);
         }
-        j += k;
-    }
+        let mut i = 0i32;
+        let (even, odd): (Vec<f64>,Vec<f64>) = vec.into_iter().partition(|_: &f64| {
+            let ret = i % 2 == 0;
+            i +=  1;
+            return ret;
+        });
 
-    /* Compute the FFT */
-    c1 = -1.0;
-    c2 = 0.0;
-    l2 = 1;
-    for l in 0..m {
-      l1 = l2;
-      l2 <<=  1;
-      u1 = 1.0;
-      u2 = 0.0;
-      for j in 0..l1 {
-        for i in (j..nn).step_by(l2) {
-            i1 = i + l1;
-            t1 = u1 * x[i1] - u2 * y[i1];
-            t2 = u1 * y[i1] + u2 * x[i1];
-            x[i1] = x[i] - t1;
-            y[i1] = y[i] - t2;
-            x[i] += t1;
-            y[i] += t2;
-         }
-         z =  u1 * c1 - u2 * c2;
-         u2 = u1 * c2 + u2 * c1;
-         u1 = z;
-      }
-      c2 = (1.0 - c1).sqrt() / 2.0;
-      if dir == 1 {
-            c2 = -c2;
-      }
+        let mut fft_even = self.execute_real(even);
+        let mut fft_odd = self.execute_real(odd);
 
-      c1 = (1.0 + c1).sqrt() / 2.0;
-    }
-
-    /* Scaling for forward transform */
-    if dir == 1 {
-        for i in 0..nn {
-            x[i] = x[i] / nn as f64;
-            y[i] = y[i] / nn as f64;
+        for e in fft_even.clone().iter() {
+            fft_even.push(*e);
         }
-    }
-    true
-}
-
-// -------------------------------------------------------------------------
-//   Perform a 2D FFT inplace given a complex 2D array
-//   The direction dir, 1 for forward, -1 for reverse
-//   The size of the array (nx,ny)
-//   Return false if there are memory problems or
-//   the dimensions are not powers of 2
-
-fn fft2D(c: &mut Vec<Complex64>, nx: i32, ny: i32, dir: i32) -> bool {
-    let mut m: i32 = 0;
-    let mut twopm: i32 = 0;
-
-    let mut real: Vec<f64> = vec![0.0; nx as usize];
-    let mut imag: Vec<f64> = vec![0.0; nx as usize];
-
-    /* Transform the rows */
-    let (m, twopm, res) = power_of_2(nx);
-    // println!("nx     m: {}, twopm: {}, res: {}", m, twopm, res);
-    if !res || twopm != nx {
-        return false;
-    }
-    let mut idx: usize = 0;
-    for j in 0..ny {
-        for i in 0..nx {
-            idx = (j*nx + i) as usize;
-            real[i as usize] = c[idx].re;
-            imag[i as usize] = c[idx].im;
-        //    println!("nx - first for  i: {}, idx: {} ", i, idx);
+        for o in fft_odd.clone().iter() {
+            fft_odd.push(*o);
         }
-        let a = fft(dir, m, &mut real, &mut imag);
-        println!("nx - res of fft():  {}", a);
-        for i in 0..nx {
-            idx = (j*nx + i) as usize;
-            c[idx].re = real[i as usize];
-            c[idx].im = imag[i as usize];
-            // println!("nx - second for  i: {}, idx: {} ", i, idx);
+
+        let mut output = Vec::new();
+
+        for i in 0..length {
+            let c = fft_even[i] + self.omega(-(i as f64), length as f64) * fft_odd[i];
+            output.push(c);
         }
+
+        return output;
     }
 
-    real = vec![0.0; ny as usize];
-    imag = vec![0.0; ny as usize];
-
-    /* Transform the columns */
-    let (m, twopm, res) = power_of_2(ny);
-    // println!("ny     m: {}, twopm: {}, res: {}", m, twopm, res);
-    if !res || twopm != ny {
-        return false;
+    pub fn omega(&self, k: f64, n: f64) -> Complex64 {
+        // e^(i2πk/n) = cos(2πk/n) + sin(2πk/n)i
+        let theta = (2.0 * 3.141592653589793238460 * k) / n;
+        Complex64::new(theta.cos(), theta.sin())
     }
-    for i in 0..nx {
-        for j in 0..ny {
-            idx = (j*nx + i) as usize;
-            // println!("ny - first for  i: {}, idx: {} ", i, idx);
-            real[j as usize] = c[idx].re;
-            imag[j as usize ] = c[idx].im;
-        }
-        let res = fft(dir, m, &mut real, &mut imag);
-         println!("ny - res of fft():  {}", res);
-        for j in 0..ny {
-            idx = (j*nx + i) as usize;
-            c[idx].re = real[j as usize];
-            c[idx].im = imag[j as usize];
-            // println!("ny - second for  i: {}, idx: {} ", i, idx);
-        }
-    }
-    true
 }
